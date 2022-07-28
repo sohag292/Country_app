ba3ee22eb46d7ace1a2751ce737958c3bd51b9f9
sohag292 <sohagshah571@gmail.com>
2022-07-29 01:19:41 +0600

    first commit

--- src/countury/Country.js
+++ src/countury/Country.js
@@ -3,8 +3,9 @@ import style from './country.module.css'
 export default function Country(props) {
     // const {country} = props;
     const {name, flags, capital, population, area} = props.country;
-  const onhandleName= (name) =>{
-    props.onRemoveCountries(name);
+    
+  const handleRemoveCountry= (name) =>{
+    props.onRemoveCountry(name);
   }
   return (
     <article className={style.country}> 
@@ -14,7 +15,9 @@ export default function Country(props) {
             <h3>Population: {population}</h3>
             <h3>Capital: {capital}</h3>
             <h3>Area: {area}</h3>
-            <button className={style.btn} onClick={()=>{onhandleName(name.common)}}>Remove Countery</button>
+            <button className={style.btn} onClick={()=>{
+              handleRemoveCountry(name.common)
+              }}>Remove Countery</button>
         </div>
     </article>
   )
